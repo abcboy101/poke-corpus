fd421fb028cd8a6e0934128b7dec796e792e1fe4
abcboy101 <16735361+abcboy101@users.noreply.github.com>
2025-06-09 17:14:34 -0400

    Refactor test to use cases

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,78 +1,38 @@
 import { formatBytes, formatPercent } from './utils';
 
-test('bytesToUnits, 0 bytes', () => {
-  const n = 0;
-  const { amount: value, formatParams: {amount: options} } = formatBytes(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('0 bytes');
+const bytesToUnitsTestCases: [number, string][] = [
+  [0, '0 bytes'],
+  [1, '1 byte'],
+  [10, '10 bytes'],
+  [999, '999 bytes'],
+  [1000, '1.0 kB'],
+  [10_000, '10.0 kB'],
+  [100_000_000, '100 MB'],
+];
+
+describe("bytesToUnits", () => {
+  test.each(bytesToUnitsTestCases)(
+    "%c%s", (n, s) => {
+      const { amount: value, formatParams: {amount: options} } = formatBytes(n);
+      const format = Intl.NumberFormat('en-US', options).format(value);
+      expect(format).toEqual(s);
+    }
+  );
 });
 
-test('bytesToUnits, 1 byte', () => {
-  const n = 1;
-  const { amount: value, formatParams: {amount: options} } = formatBytes(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('1 byte');
-});
+const formatPercentTestCases: [number, string][] = [
+  [0.01, '1.0%'],
+  [0.50049, '50.0%'], // round down
+  [0.50051, '50.1%'], // round up
+  [1, '100.0%'],
+];
 
-test('bytesToUnits, 10 bytes', () => {
-  const n = 10;
-  const { amount: value, formatParams: {amount: options} } = formatBytes(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('10 bytes');
-});
-
-test('bytesToUnits, 999 bytes', () => {
-  const n = 999;
-  const { amount: value, formatParams: {amount: options} } = formatBytes(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('999 bytes');
-});
-
-test('bytesToUnits, 1.0 kB', () => {
-  const n = 1000;
-  const { amount: value, formatParams: {amount: options} } = formatBytes(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('1.0 kB');
-});
-
-test('bytesToUnits, 10.0 kB', () => {
-  const n = 10_000;
-  const { amount: value, formatParams: {amount: options} } = formatBytes(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('10.0 kB');
-});
-
-test('bytesToUnits, 100 MB', () => {
-  const n = 100_000_000;
-  const { amount: value, formatParams: {amount: options} } = formatBytes(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('100 MB');
-});
-
-test('formatPercent, 1.0%', () => {
-  const n = 0.01;
-  const { amount: value, formatParams: {amount: options} } = formatPercent(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('1.0%');
-});
-
-test('formatPercent, 50.0% (round down)', () => {
-  const n = 0.50049;
-  const { amount: value, formatParams: {amount: options} } = formatPercent(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('50.0%');
-});
-
-test('formatPercent, 50.1% (round up)', () => {
-  const n = 0.50051;
-  const { amount: value, formatParams: {amount: options} } = formatPercent(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('50.1%');
-});
-
-test('formatPercent, 100.0%', () => {
-  const n = 1;
-  const { amount: value, formatParams: {amount: options} } = formatPercent(n);
-  const format = Intl.NumberFormat('en-US', options).format(value);
-  expect(format).toEqual('100.0%');
+describe("formatPercent", () => {
+  test.each(formatPercentTestCases)(
+    "%c%s", (n, s) => {
+      const { amount: value, formatParams: {amount: options} } = formatPercent(n);
+      const format = Intl.NumberFormat('en-US', options).format(value);
+      expect(format).toEqual(s);
+    }
+  );
 });
